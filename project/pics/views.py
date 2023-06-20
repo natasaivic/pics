65cf7f94f86dbc7b84a76a625d05bc41cda8677c
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2023-06-20 14:49:55 -0700

    Creating templates with basic tags such as headlines and enhancing them by incorporating base.html to demonstrate inheritance using Django.

--- project/pics/views.py
+++ project/pics/views.py
@@ -6,17 +6,21 @@ def index(request):
     return HttpResponse("The project aims to develop an Instagram-like app using Python, Django, PostgreSQL, Django REST Framework, HTML/CSS, React, and Bootstrap.")
 
 
+# The initial step in creating a new account on the platform. With this method, new users can sign up and provide the necessary information to create their Pics account. 
 def registration(request):
-    return HttpResponse("Here is the initial step in creating a new account on the platform. It is the page where new users can sign up and provide the necessary information to create their Pics account. ")
+    return render(request, 'pics/registration.html')
 
 
+# This method renders the page that appears after the registration process, or when users log out and want to log back in to their account. 
 def login(request):
-    return HttpResponse("This is the page that appears after the registration process, or when users log out and want to log back in to their account. ")
+    return render(request, 'pics/login.html')
 
 
+# This method renders the profile page displays a grid of images as a visual representation of the user's posts.
 def profile(request):
-    return HttpResponse("The profile page displays a grid of images as a visual representation of the user's posts.")
+    return render(request, 'pics/profile.html')
 
 
+# The news feed displays photos shared by the accounts that the user follows.
 def feed(request):
-    return HttpResponse("The news feed displays photos shared by the accounts that the user follows.")
+    return render(request, 'pics/feed.html')
